8eda9f57c9069d84d146e086c84abfe196815e5f
Alexis LG <alexis.legoadec@laposte.net>
2014-05-25 02:20:15 +0200

    Initial import

--- javascript/RorSquare.js
+++ javascript/RorSquare.js
@@ -131,7 +131,7 @@ function genereGrilleValide() {
 	
 	for(var i=0; i < matrice.length - 1; i++) {
 		for(var j=0; j < matrice.length - 1; j++) {
-			matrice[i][j] = ( Math.floor( Math.random() * 10 ) ) % ( squareNumber - minimumNbr( lireSommeY(j), lireSommeY(i) ) );
+			matrice[i][j] = ( Math.floor( Math.random() * 10 ) ) % ( squareNumber - minimumNbr( lireSommeY(i), lireSommeY(j) ) );
 		}
 	}
 	
